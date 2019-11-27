81a1d4860feb1ad57d363a67454b4f97cd5a993d
Patrick <patrickpetropoulos@gmail.com>
2019-11-27 18:57:32 -0500

    Initial Project Setup

---
+++ Database/RankkerData/dbo/Tables/MovieGenre.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[MovieGenre]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [SourceId] INT NOT NULL,
+	CONSTRAINT [moviegenre_namesourceid] UNIQUE NONCLUSTERED (
+		[Name], [SourceId]
+	)
+)
+
