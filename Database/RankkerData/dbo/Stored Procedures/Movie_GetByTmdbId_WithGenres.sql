b6f98d589445d992c8d8590f28ab45009c1b12d4
Patrick <patrickpetropoulos@gmail.com>
2020-03-16 10:29:01 -0400

    Fix typo

--- Database/RankkerData/dbo/Stored Procedures/Movie_GetByTmdbId_WithGenres.sql
+++ Database/RankkerData/dbo/Stored Procedures/Movie_GetByTmdbId_WithGenres.sql
@@ -17,12 +17,12 @@ Begin
       ,m.[TmdbBackdropPath]
       ,m.[Status]
       ,m.[DateUpdated]
-  FROM [Rankker].[dbo].[Movie] m
+  FROM [dbo].[Movie] m
   where m.[TmdbId] = @tmdbId 
 
   select m.[Id] as MovieId, g.[Id] as MovieGenreId, g.[Name] as MovieGenreName
-  from [Rankker].[dbo].[MovieGenre] g
-  inner  join [Rankker].[dbo].[MovieGenreRel] rel on rel.MovieGenreId = g.Id
-  inner join [Rankker].[dbo].[Movie] m on rel.MovieId = m.Id
+  from [dbo].[MovieGenre] g
+  inner  join [dbo].[MovieGenreRel] rel on rel.MovieGenreId = g.Id
+  inner join [dbo].[Movie] m on rel.MovieId = m.Id
   where m.TmdbId = @tmdbId 
 end
